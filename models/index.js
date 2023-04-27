39d99bd1cd868f32e52db801c64503fe77203489
JoeVictor <joaoriolima@gmail.com>
2023-04-27 23:10:25 +0800

    just a few changes

--- models/index.js
+++
@@ -1,5 +0,0 @@
-const User = require('./User');
-const Quest = require('./Quest');
-const Badge = require('./Badge');
-
-module.exports = { User, Quest, Badge };
---
+++ server/models/Index.js
@@ -0,0 +1,5 @@
+const User = require('./user');
+const Quest = require('./quest');
+const Badge = require('./badge');
+
+module.exports = { User, Quest, Badge };
