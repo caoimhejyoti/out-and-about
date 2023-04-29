b188765a97c44b32c8e8b54cb86f201357d8450c
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-29 17:09:50 +0800

    creating base files

---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Map from "../components/maps/mandurah-foreshore-map";
+
+const Home = () => {
+  return (
+    <main>
+      <div className="flex-row justify-center">
+        <Map />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
