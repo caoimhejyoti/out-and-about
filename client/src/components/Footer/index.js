8c60286d51fc1f0f69a6490e68b053aadfac962d
Stephanee Hayden <stephaneehayden@gmail.com>
2023-04-30 14:58:07 +1000

    updated footer section

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,17 +1,63 @@
 import React from "react";
+import github from "../../assets/github.png";
+import Box from "@mui/material/Box";
 
 const Footer = () => {
+  const container = {
+    justifyContent: "center",
+  };
   const styleObj = {
+    width: "150px",
     color: "white",
+    target: "_blank",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+  };
+  const githubStyle = {
+    marginRight: "10px",
+    width: "20px",
+    height: "20px",
   };
 
   return (
-    <footer className="w-100 mt-auto">
-      <div className="container text-center">
-        <h6 style={styleObj}>
-          Proudly created by: Caoimhe, Harry, Joao and Steph!
-        </h6>
-      </div>
+    <footer className="w-100 mt-auto flex-row" style={container}>
+      <a
+        href="https://github.com/caoimhejyoti"
+        target="_blank"
+        style={styleObj}
+        rel="noreferrer"
+      >
+        <img style={githubStyle} src={github} alt="github" />
+        <h6>Caoimhe</h6>
+      </a>
+      <a
+        href="https://github.com/HarryWard-15"
+        target="_blank"
+        style={styleObj}
+        rel="noreferrer"
+      >
+        <img style={githubStyle} src={github} alt="github" />
+        <h6>Harry</h6>
+      </a>
+      <a
+        href="https://github.com/JoeVictor"
+        target="_blank"
+        style={styleObj}
+        rel="noreferrer"
+      >
+        <img style={githubStyle} src={github} alt="github" />
+        <h6>Joe</h6>
+      </a>
+      <a
+        href="https://github.com/stephaneeh"
+        target="_blank"
+        style={styleObj}
+        rel="noreferrer"
+      >
+        <img style={githubStyle} src={github} alt="github" />
+        <h6>Steph</h6>
+      </a>
     </footer>
   );
 };
