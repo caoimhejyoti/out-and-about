8d99b40b0ef25560a885a4f633784d75cfb7430b
caoimhejyoti <caoimhejyoti@gmail.com>
2023-05-01 13:33:38 +0800

    formatting alert with markers

--- client/src/components/maps/mandurah-forshore.js
+++ client/src/components/maps/mandurah-forshore.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from "react";
-// import { ReactDOM } from "react";
+import ReactDOM from "react-dom";
 import mapboxgl from "mapbox-gl";
 import geoJSON from "./map-data/mandurah-foreshore-markers.json";
 
@@ -7,6 +7,17 @@ import "./../../style/map.css";
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiY2FvaW1oZWp5b3RpIiwiYSI6ImNsaDM1OTdoNzFqdHczY3BudXd0d3M4enMifQ.862ifcHz_-veRJKGrWjwQw";
+
+const Marker = ({ onClick, children, feature }) => {
+  const _onClick = () => {
+    onClick(feature.properties.name);
+  };
+  return (
+    <button onClick={_onClick} className="marker">
+      {children}
+    </button>
+  );
+};
 
 export default function MandurahMap() {
   const mapContainer = useRef(null);
@@ -27,12 +38,24 @@ export default function MandurahMap() {
     });
 
     // Create default markers
-    geoJSON.features.map((feature) =>
-      new mapboxgl.Marker().setLngLat(feature.geometry.coordinates).addTo(map)
-    );
+    geoJSON.features.forEach((feature) =>{
+        const ref = React.createRef();
+        ref.current = document.createElement("div");
+
+        ReactDOM.render(
+            <Marker onClick={markerClicked} feature={feature} />,
+            ref.current
+        );
+
+      new mapboxgl.Marker(ref.current).setLngLat(feature.geometry.coordinates).addTo(map);
+  });
 
     return () => map.remove();
   }, []);
+
+  const markerClicked = (title) => {
+    window.alert(title);
+  }
 
   return (
     <div>
