24e38acf6a2a8cdbfe17fc0490124aece9d838f2
caoimhejyoti <caoimhejyoti@gmail.com>
2023-05-01 21:57:00 +0800

    login and sign up working!!!

--- server/models/tier.js
+++ server/models/tier.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
 const tierSchema = new Schema({
-    name: {
-     type: Number,
-     required: true,
-     trim: true
-   },
-   description: {
-     type: String
-   }
- });
+  name: {
+    type: Number,
+    required: true,
+    trim: true,
+    default: 1,
+  },
+  description: {
+    type: String,
+  },
+});
 
-const Tier = mongoose.model('Tier', tierSchema);
+const Tier = mongoose.model("Tier", tierSchema);
 
 module.exports = Tier;
