9e7c710e6c9fd5c99869f306c8ca26698f20f049
stephaneeh <stephaneehayden@gmail.com>
2023-05-07 17:09:45 +1000

    updates to - profile - dashboard - login/logout - signup

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,17 +2,15 @@ import React from "react";
 
 import MandurahMap from "./maps/mandurah-forshore";
 import UwaMap from "./maps/uwa-grad";
+import Hero from "./Hero";
+import heroImg from "../assets/hero.png";
 
 const Home = () => {
   return (
     <main>
       <div className="flex-row justify-center">
         <div className="col-12 col-md-8">
-          <p>Hello World!</p>
-          <MandurahMap />
-        </div>
-        <div className="col-12 col-md-8">
-          <UwaMap />
+          <Hero />
         </div>
       </div>
     </main>
