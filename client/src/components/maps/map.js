587b2207fa1707621313fed5d4424bde877af3d2
Harry_Desktop <hward.1508@gmail.com>
2023-05-09 23:25:41 +0800

    map changing

---
+++ client/src/components/maps/map.js
@@ -0,0 +1,24 @@
+import Uwamap from "./uwa-grad"
+import MandurahMap from "./mandurah-forshore"
+import { QUERY_USER, QUERY_ME } from "../../utils/queries";
+import React, { useState } from "react";
+import { Navigate, useParams } from "react-router-dom";
+import { useQuery, useMutation } from "@apollo/client";
+
+export default function Map() {
+    const { username: userParam } = useParams();
+    const { loading, data } = useQuery(QUERY_ME, {
+        variables: { username: userParam },
+      });
+
+    const tier = data.me.currentQuest.tierName;
+
+    console.log(tier);
+
+    if(tier === 'Pedestrian') {
+        return MandurahMap();
+    }
+    else if(tier === 'Rambler') {
+        return Uwamap();
+    }
+}
