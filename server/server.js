eee74a5fc61960a96d3ef10b37745a8b2f29415c
caoimhejyoti <caoimhejyoti@gmail.com>
2023-05-10 15:24:12 +0800

    reverting to lowercase filenames

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
-// const { authMiddleware } = require("./utils/auth");
-const { authMiddleware } = require("./Utils/Auth.js");
+const { authMiddleware } = require("./utils/auth");
+// const { authMiddleware } = require("./Utils/Auth.js");
 
 const { typeDefs, resolvers } = require("./schema");
 const db = require("./config/connection");
