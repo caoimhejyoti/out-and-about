3f03223f92fcb54009dce71ad5a9a3d3b06aa1fc
caoimhejyoti <caoimhejyoti@gmail.com>
2023-05-10 14:17:58 +0800

    adding .js to auth import

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
-const { authMiddleware } = require("./utils/auth");
+// const { authMiddleware } = require("./utils/auth");
+const { authMiddleware } = require("./utils/auth.js");
 
 const { typeDefs, resolvers } = require("./schema");
 const db = require("./config/connection");
