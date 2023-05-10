66d390c676f80ae42273ae7126cdfd12368a4a19
stephaneeh <stephaneehayden@gmail.com>
2023-05-10 23:31:24 +1000

    fixing dupe bug

--- client/src/components/qrcode_button.js
+++ client/src/components/qrcode_button.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
-import { useNavigate } from "react-router-dom";
 import {
   UPDATE_USER_BADGE,
   UPDATE_STATUS,
