5f545f6d3c8ea6914757dfaaa3d2f3dce45726ee
Harry_Desktop <hward.1508@gmail.com>
2023-06-20 18:17:38 +0800

    heroku changes

--- client/src/components/Profile/DeleteUserModal.js
+++ client/src/components/Profile/DeleteUserModal.js
@@ -63,10 +63,11 @@ const DeleteUser = () => {
     // console.log("formstate email", formState.email); //used for debugging
     if (formState.correctEmail === formState.email) {
       console.log("in if statement");
-      navigate("/");
+      navigate("/signup");
       const { data } = await deleteUserProfile({
         variables: { Id: formState.userId },
       });
+      Auth.logout();
     } else {
       console.log("FAILED DELETE");
       e.preventDefault();
